c618b04f54eb179709c3ef8989a22df8a1702884
AndreClerigo Laptop <andreclerigo@outlook.com>
2021-10-27 11:01:24 +0100

    rc1 pratica3

--- IA/Prática/guiao2/tree_search.py
+++ IA/Prática/guiao2/tree_search.py
@@ -57,28 +57,33 @@ class SearchProblem:
         self.domain = domain
         self.initial = initial
         self.goal = goal
+
     def goal_test(self, state):
         return self.domain.satisfies(state,self.goal)
 
 # Nos de uma arvore de pesquisa
 class SearchNode:
-    def __init__(self,state,parent): 
+    def __init__(self, state, parent, depth): 
         self.state = state
         self.parent = parent
+        self.depth = depth
+
     def __str__(self):
         return "no(" + str(self.state) + "," + str(self.parent) + ")"
+
     def __repr__(self):
         return str(self)
 
 # Arvores de pesquisa
 class SearchTree:
     # construtor
-    def __init__(self,problem, strategy='breadth'): 
+    def __init__(self, problem, strategy='breadth'): 
         self.problem = problem
-        root = SearchNode(problem.initial, None)
+        root = SearchNode(problem.initial, None, 0)
         self.open_nodes = [root]
         self.strategy = strategy
         self.solution = None
+        self.length = None
 
     # obter o caminho (sequencia de estados) da raiz ate um no
     def get_path(self,node):
@@ -94,12 +99,14 @@ class SearchTree:
             node = self.open_nodes.pop(0)
             if self.problem.goal_test(node.state):
                 self.solution = node
+                self.length = self.solution.depth
                 return self.get_path(node)
             lnewnodes = []
             for a in self.problem.domain.actions(node.state):
-                newstate = self.problem.domain.result(node.state,a)
-                newnode = SearchNode(newstate,node)
-                lnewnodes.append(newnode)
+                newstate = self.problem.domain.result(node.state ,a)
+                if newstate not in self.get_path(node):
+                    newnode = SearchNode(newstate, node, node.depth + 1)
+                    lnewnodes.append(newnode)
             self.add_to_open(lnewnodes)
         return None
 
@@ -111,4 +118,3 @@ class SearchTree:
             self.open_nodes[:0] = lnewnodes
         elif self.strategy == 'uniform':
             pass
-
